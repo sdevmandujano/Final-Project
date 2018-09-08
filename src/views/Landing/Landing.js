6eba19721c57edb80d0bdcda0472076caf6d1280
Veritosvb <vero.velez@gmail.com>
2018-09-08 11:30:11 -0500

    some landing psge

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,76 +1,71 @@
 import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-// @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-
-// @material-ui/icons
-
-// core components
-import Header from "components/Header/Header.jsx";
-import Footer from "components/Footer/Footer.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import Button from "../components/CustomButtons/Button.jsx";
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
-
-// Sections for this page
-
-const dashboardRoutes = [];
+import Jumbotron from "../../components/Jumbotron"
+  
 
 class Landing extends React.Component {
   render() {
-    const { classes, ...rest } = this.props;
+ 
     return (
       <div>
-        <Header
-          color="transparent"
-          routes={dashboardRoutes}
-          brand="Material Kit React"
-          rightLinks={<HeaderLinks />}
-          fixed
-          changeColorOnScroll={{
-            height: 400,
-            color: "white"
-          }}
-          {...rest}
-        />
-        <Parallax filter image={require("../../assets/img/bg.jpg")}>
-          <div className={classes.container}>
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={6}>
-                <h1 className={classes.title}>Your Story Starts With Us.</h1>
-                <h4>
-                  Every landing page needs a small description after the big
-                  bold title, that's why we added this text here. Add here all
-                  the information that can make you or your product create the
-                  first impression.
-                </h4>
-                <br />
-                <Button
-                  color="danger"
-                  size="lg"
-                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fas fa-play" />Watch video
-                </Button>
-              </GridItem>
-            </GridContainer>
+      <Jumbotron className="masthead">
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-9 mx-auto">
+            <h1 className="mb-5">Yell</h1>
           </div>
-        </Parallax>
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div className={classes.container}>
-         
-          
+          <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+            <form>
+              <div className="form-row">
+                <div className="col-12 col-md-9 mb-2 mb-md-0">
+                  <input type="email" className="form-control form-control-lg" placeholder="Enter your email..."/>
+                </div>
+                <div class="col-12 col-md-3">
+                  <button type="submit" className="btn btn-block btn-lg btn-primary">Entrar</button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-        <Footer />
       </div>
+      </Jumbotron>
+      
+      <section className="features-icons bg-light text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div className="features-icons-icon d-flex">
+                <i className="icon-screen-desktop m-auto text-primary"></i>
+              </div>
+              <h3>Arma tu perfil</h3>
+              <p className="lead mb-0">This theme will look great on any device, no matter the size!</p>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div className="features-icons-icon d-flex">
+                <i className="icon-layers m-auto text-primary"></i>
+              </div>
+              <h3>Recluta</h3>
+              <p className="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+              <div className="features-icons-icon d-flex">
+                <i className="icon-check m-auto text-primary"></i>
+              </div>
+              <h3>Evalua</h3>
+              <p className="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
     );
   }
 }
 
-export default withStyles(Landing);
+export default Landing;
