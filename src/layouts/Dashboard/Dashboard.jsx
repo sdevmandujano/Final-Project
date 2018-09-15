76fe42bba098fe339644d737d02271e06827c91e
Veritosvb <vero.velez@gmail.com>
2018-09-15 14:48:15 -0500

    clean p2

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NotificationSystem from "react-notification-system";
 
-import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Nav from "../../components/Nav";
