d082064266887e48ded10ff688b1d1a311264a24
sdevmandujano <zmandujano01@gmail.com>
2018-09-20 18:23:04 -0500

    adding to component

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -13,3 +13,23 @@ class SearchResultContainer extends Component {
     this.searchGiphy();
     this.searchTop();
   }
+
+  searchGiphy = () => {
+    axios
+      .get(
+        "https://api.twitch.tv/kraken/clips/top?limit=1&game=Fortnite&trending=true",
+        {
+          headers: {
+            "Client-ID": "4sq1xbwvhlr0nn95o51t91c0pfmajm",
+            Accept: "application/vnd.twitchtv.v5+json"
+          }
+        }
+      )
+      .then(result => {
+        console.log(result);
+        this.setState({ data: result.data.clips[0] });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
